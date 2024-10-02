c5961c01d4d8fb07802aeb0aae4ee91c6a976250
Diogo Vieira <diogovieira708@gmail.com>
2024-10-02 19:13:49 -0300

    publicgain

---
+++ aula 2 diogo - Copia/exemplo2.py
@@ -0,0 +1,12 @@
+#Imprimir mensagem de boas-vindas personalizada
+nome = input("qual seu nome?")
+nome_do_meio = input ("qual é o seu nome do meio?")
+sobrenome = input("qual seu sobrenome?")
+#uma forma de imprimir
+print(nome)
+print(sobrenome)
+print(nome_do_meio)
+#outra forma de imprimir
+print(nome +" "+ nome_do_meio +" "+ sobrenome)
+#terceira forma de imprimir
+print(nome,nome_do_meio,sobrenome)
