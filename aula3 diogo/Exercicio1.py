8dbdbd2b5b196eeb623a92c756859328c6b8774a
Diogo Vieira <diogovieira708@gmail.com>
2024-10-02 20:56:39 -0300

    Exercício 1 Finalizado
    Programei um programa que calcula o IPVA do carro a partir do preço do carro do usuario.

---
+++ aula3 diogo/Exercicio1.py
@@ -0,0 +1,7 @@
+#faça um programa que calcule o IPVA o carro de acordo do preço do carro do úsuario
+print("boa noite! seja bem vindo ao Seguros Barra")
+print("vamos calcular o valor do seu carro")
+valordocarro = int(float(input("qual é o valor do seu carro?")))
+ipva = valordocarro / 0.04
+print(f"Sabendo que o valor do seu carro é {valordocarro}, o IPVA do seu carro será:{ipva}")
+print("Obrigado pela pefrerência!")
